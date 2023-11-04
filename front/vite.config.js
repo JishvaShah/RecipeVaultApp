2b135d05748936ade676506608a282a7696b1e5b
Jishva Shah <shah.jish@Northeastern.edu>
2023-11-04 08:26:44 -0700

    Using proxy to connect React frontend server with the backend server.

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// proxy in this is redirecting vite to the backend server 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target:"http://localhost:3000",
+        changeOrigin:true,
+        secure:false
+      }
+    }
+  }
 })
