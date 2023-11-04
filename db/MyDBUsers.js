f8a4d744f48f336c67e9a104fa7708c19e25a940
Gaurang <gaurangjotwani@gmail.com>
2023-11-04 16:39:54 -0700

    make register and login endpoints

--- db/MyDBUsers.js
+++ db/MyDBUsers.js
@@ -1,5 +1,7 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import bcrypt, { hash } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -8,7 +10,6 @@ function MyMongoDB() {
   const uri = process.env.MONGO_URL || "mongodb://127.0.0.1:27017";
 
   async function connect() {
-    console.log("here");
     const client = new MongoClient(uri, {});
     await client.connect();
     const db = client.db("RecipeVault");
@@ -17,9 +18,41 @@ function MyMongoDB() {
 
   myDB.register = async function (username, password) {
     const { client, db } = await connect();
+    const userCollection = db.collection("Users");
     try {
-      const user = await db.collection("Users").findOne({ username });
-      return user;
+      const user = await userCollection.findOne({ username });
+      if (user) {
+        return { message: "User already exists!" };
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await userCollection.insertOne({
+        username,
+        password: hashedPassword,
+      });
+      return { message: "User Registered Successfully!", user: newUser };
+    } finally {
+      await client.close();
+    }
+  };
+
+  myDB.login = async function (username, password) {
+    const { client, db } = await connect();
+    const userCollection = db.collection("Users");
+    try {
+      const user = await userCollection.findOne({ username });
+      if (!user) {
+        return { message: "User does not exists!" };
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return { message: "Username or password is Incorrect!" };
+      }
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+      return { token, userID: user._id };
     } finally {
       await client.close();
     }
--- routes/userApi.js
+++ routes/userApi.js
@@ -6,13 +6,14 @@ import myDB from "../db/MyDBUsers.js";
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  console.log("Username and Password: ", username, password);
-  const user = await myDB.register(username, password);
-  res.json(user);
+  const result = await myDB.register(username, password);
+  res.json(result);
 });
 
 router.post("/login", async (req, res) => {
-  res.json(["A", 1, "B", 2]);
+  const { username, password } = req.body;
+  const result = await myDB.login(username, password);
+  res.json(result);
 });
 
 export default router;
