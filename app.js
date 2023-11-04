25073bbc71e0ac429576a2d32e7dae9a6cbc7edc
Jishva Shah <shah.jish@Northeastern.edu>
2023-11-04 09:42:50 -0700

    Database folder creater

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cookieParser from"cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import { fileURLToPath  } from "url";
+import dataRouter from "./db/MyDB.js";
 
 //ES6 modules don't have __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -16,7 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front", "dist")));
-
 app.use("/", indexRouter);
+app.use("/data", dataRouter);
  
+
+
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 let router = express.Router();
 
+
 /* GET home page. */
 router.get('/api', function(req, res) {
   res.json(["A",1,"B",2]);
