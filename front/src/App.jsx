ec5c7af370d87c07bf8f5b144536e9d77617c246
Gaurang <gaurangjotwani@gmail.com>
2023-11-04 17:02:24 -0700

    start on frontend

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,21 @@
-export default function App(){
-  return(
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { SavedRecipes } from "./pages/saved-recipe";
+import { Auth } from "./pages/auth";
+import { CreateRecipes } from "./pages/create-recipe";
+
+export default function App() {
+  return (
     <>
       <div>
-        <h1>Hi there!</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/create-recipe" element={<CreateRecipes />} />
+            <Route path="/saved-recipes" element={<SavedRecipes />} />
+          </Routes>
+        </Router>
       </div>
     </>
   );
