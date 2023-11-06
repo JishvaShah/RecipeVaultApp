42f32e6bd8e5c70608055edd8872ed92db4621c4
Gaurang <gaurangjotwani@gmail.com>
2023-11-05 18:15:50 -0800

    add carousel

--- front/src/components/Carousel.jsx
+++ front/src/components/Carousel.jsx
@@ -1,42 +1,35 @@
+import "./Carousel.css";
+
 export const Carousel = () => {
   return (
     <>
-      <div id="carouselExample" className="carousel slide">
+      <div
+        id="carouselExampleAutoplaying"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="..." className="d-block w-100" alt="..." />
+            <img
+              src="https://hips.hearstapps.com/del.h-cdn.co/assets/15/39/2560x1280/landscape-1443212749-gettyimages-73042755.jpg?resize=1200:*"
+              className="d-block w-100 img-fluid maxH"
+              alt="Picture of Bread"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h1 className="headingStyle">Welcome to Recipe Vault!</h1>
+            </div>
           </div>
           <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
+            <img
+              src="https://assets-eu-01.kc-usercontent.com/559bb7d3-88a4-01c1-79a3-dd4d5b2d2bb0/760d10cf-7037-481e-9f6a-5b8d6a408ef1/Biscoff%20Cupcakes%201_landscape.jpg"
+              className="d-block w-100 maxH"
+              alt="Cupcakes"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h1 className="headingStyle">Create your own Recipes</h1>
+            </div>
           </div>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </>
   );
--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -1,3 +1,14 @@
+import { Carousel } from "../components/Carousel";
+import { RecipeGallery } from "../components/RecipeGallery";
+
 export const Home = () => {
-  return <div> Home </div>;
+  return (
+    <div className="mt-4 ms-4 me-4">
+      <Carousel />
+      <div className="d-flex justify-content-center">
+        <h1 className="text-center mt-4">My Recipes!</h1>
+      </div>
+      <RecipeGallery />
+    </div>
+  );
 };
